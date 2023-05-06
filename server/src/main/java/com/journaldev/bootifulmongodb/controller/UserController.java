be1c8da301481275760cde1b71c2c78f4bb4e31a
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-05-05 21:27:48 -0700

    backerserver

---
+++ server/src/main/java/com/journaldev/bootifulmongodb/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.journaldev.bootifulmongodb.controller;
+
+import com.journaldev.bootifulmongodb.dal.UserRepository;
+import com.journaldev.bootifulmongodb.model.User;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/")
+public class UserController {
+    private final Logger LOG = (Logger) LoggerFactory.getLogger(getClass());
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        LOG.info("Getting all users.");
+        return userRepository.findAll();
+    }
+
+}

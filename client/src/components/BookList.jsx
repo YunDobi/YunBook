e0b3120379520b7b05492b68dc4115aaa998f9ca
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-08-06 08:08:37 -0700

    client fixed

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const BookList = ({ books }) => {
+  const navigate = useNavigate();
   // console.log("books", books)
   return books.map((book, index) => {
     return (
       <>
-        <li key={index}>{book.volumeInfo.title}</li>
-        <img src={book.volumeInfo.imageLinks.thumbnail} alt='' />
+        {/* <li key={index}> */}
+        <div>
+          <h3>{book.volumeInfo.title}</h3>
+          <div style={{ display: 'flex' }}>
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt='' />
+            <div>
+              <p key={index + ' authors'}>{book.volumeInfo.authors}</p>
+              <p>{book.volumeInfo.description}</p>
+            </div>
+            <a href={book.volumeInfo.canonicalVolumeLink}>Link to Google</a>
+            {/* <button
+              type='button'
+              onClick={() => {
+                navigate(book.volumeInfo.canonicalVolumeLink);
+              }}
+            >
+              Link to Goolge link
+            </button> */}
+          </div>
+        </div>
+        {/* </li> */}
       </>
     );
   });
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
-import Logo from '../imges/logo-no.png'
-import axios from 'axios'
-
+import React from 'react';
+import Logo from '../imges/logo-no.png';
+import { useNavigate } from 'react-router-dom';
+import './Home.css'
 
 export const Login = () => {
 
-  // const LoginSubmit = async (e) => {
-  //   const 
-  // }
+  const navigate = useNavigate()
+
 
   return (
-      <div style={{border: "1px solid black", height: '600px', width: '400px', margin: 'auto', marginTop: '30px', backgroundColor: 'black', alignItems: 'center', display: 'flex', flexDirection: "column", justifyContent: 'center'}}>
-        <img src={Logo} alt="" style={{width: '100px'}}/>
-        <p style={{color: 'white'}}>Login or Register</p>
-        <input type="text" />
-        <input type="text" />
-        <input type="button" value="login" />
+    <div   style={{
+      border: '1px solid black',
+      height: '600px',
+      width: '400px',
+      margin: 'auto',
+      marginTop: '30px',
+      backgroundColor: 'black',
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    }}>
+      <form
+        action='submit'
+        method='post'
+        style={{ display: 'flex', flexDirection: 'column' }}
+        // onSubmit={RegisterSubmit}
+      >
+        <input type='text' name='email' />
+        <input type='text' name='password' /><br/>
+        <input type='submit' value='Login' />
+        {/* <input type='button' value='Register' /> */}
+      </form>
+      <p style={{color: 'white'}}>If you are not resgister yet, <span style={{textDecoration: 'underline'}} onClick={() => {navigate('/register')}}>Sing up</span></p>
     </div>
-  )
-}
+  );
+};

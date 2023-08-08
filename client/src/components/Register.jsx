601a1ad26fabbc40c6144d45724daaf9e5488fb6
Yunchoel Lee <yun@Yunchoels-Air.attlocal.net>
2023-08-07 23:27:32 -0700

    30 sec auth when login with useeffect and client

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,48 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../imges/logo-no.png';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
 
 export const Register = () => {
-  const [userInfo, setUserInfo] = useState({})
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(loading);
+  }, [loading]);
+
   const RegisterSubmit = async (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
 
     try {
-      const requestion = axios.post('/api/users/register', {email: email, password: password});
-      const data = await requestion
-      console.log("data", data)
-      setUserInfo(data.data)
+      setLoading(true);
+      const requestion = axios.post('/api/users/register', {
+        email: email,
+        password: password,
+      });
+      const data = await requestion;
+      setLoading(false);
+      // console.log("data", data.data.doc)
+      alert(data.data.message);
+      // setUserInfo(data.data)
     } catch (error) {
-      console.error(error)
+      console.error(error);
+      alert(error.response.data.message);
     }
   };
-  console.log(Object.keys(userInfo))
+  // console.log(Object.keys(userInfo))
   return (
-    <div
-      style={{
-        border: '1px solid black',
-        height: '600px',
-        width: '400px',
-        margin: 'auto',
-        marginTop: '30px',
-        backgroundColor: 'black',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
+    <div className='loginBody'>
       <img src={Logo} alt='' style={{ width: '120px' }} />
       <p style={{ color: 'white' }}>Login or Register</p>
 
-      <form action='submit' method='post' style={{display:'flex', flexDirection: 'column'}} onSubmit={RegisterSubmit}>
+      <form
+        action='submit'
+        method='post'
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={RegisterSubmit}
+      >
         <input type='text' name='email' />
         <input type='text' name='password' />
         <input type='submit' value='Register' />
       </form>
-      {Object.keys(userInfo).length !== 0 ? (<p style={{ color: 'white' }}>Sucessfully registered!</p>) : (<p style={{ color: 'white' }}>not yet</p>)}
+      <p style={{ color: 'white' }}>
+        If you already registered,{' '}
+        <span
+          style={{ textDecoration: 'underline' }}
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          Sing in
+        </span>
+      </p>
     </div>
   );
 };

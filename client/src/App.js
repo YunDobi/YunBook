0b9221ccc5b6a9b895345d7bd474fdc242ac5348
Yunchoel Lee <yun@Yunchoels-Air.attlocal.net>
2023-08-17 04:23:33 -0700

    deleting unuse components

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './components/Home';
 import { Header } from './components/Header';
-import { Footer } from './components/Footer';
 import { Login } from './components/Login';
 import './App.css';
 import { Register } from './components/Register';
@@ -23,7 +22,6 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/search' element={<BookList />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
